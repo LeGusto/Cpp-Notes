d4b368482564acb00d51777727987793d901b053
= <janusonis.gustas@gmail.com>
2026-01-09 23:12:28 +0200

    string matching algorithms

---
+++ cpp/algorithms/KMP.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::string s;
+    std::cin>>s;
+    std::string goal;
+    std::cin>>goal;
+
+    int n = s.size();
+    int m = goal.size();
+
+    std::vector<int> LPS(m, 0);
+
+    int len = 0;
+
+    for (int i = 1; i < m; i++) {
+        while (len > 0 && goal[i] != goal[len]) {
+            len = LPS[len - 1];
+        }
+        if (goal[i] == goal[len]) {
+            LPS[i] = len + 1;
+            len++;
+        }
+    }
+
+    int LPS_p = 0;
+    int ans = 0;
+
+    for (int i = 0; i < n; i++) {
+        while (LPS_p > 0 && goal[LPS_p] != s[i]) LPS_p = LPS[LPS_p - 1];
+        if (s[i] == goal[LPS_p]) {
+            if (LPS_p < m - 1) {
+                LPS_p++;
+            } else {
+                LPS_p = LPS[LPS_p]; 
+                ans++;
+            }
+        } 
+    }
+
+    std::cout<<ans<<"\n";
+
+    
+}
